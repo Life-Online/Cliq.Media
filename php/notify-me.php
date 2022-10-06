69673199e2b2ea3593f49436dc3f1347bb253fa1
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-10-06 12:28:00 +0100

    update contact information

--- php/notify-me.php
+++ php/notify-me.php
@@ -6,7 +6,7 @@ header('content-type: application/json');
 	$o->status = 'success';
 	echo json_encode($o);
 
-	$email_to = 'joshua@lifeonline.com.ng'; // Replace by your email address
+	$email_to = 'cliqng@gmail.com'; // Replace by your email address
 	$email = $_POST["email"];
 	$text = "Congratulations ! A new person wants to be alerted when your site will be online: $email";
 
